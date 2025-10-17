50caca41de63855a2834684b5099edb19ba9dd00
daddyveee <vishooverma@gmail.com>
2025-10-17 14:31:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const currentAudio = []
+const buttons = document.querySelectorAll('.btn').forEach(button=>{
+	button.addEventListener('click', function(){
+		const audio = new Audio(this.dataset.sound);
+		audio.play();
+		currentAudio.push(audio)
+		
+	});
+
+	const stop = document.querySelector('.stop')
+
+	stop.addEventListener('click', function(){
+		currentAudio.forEach(audio=>{
+			audio.pause();
+			audio.currentTime=0;
+		});
+		currentAudio=[]
+	})
+})
